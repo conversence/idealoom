ce57b28f998bf5f919b477194ffb9435714ca400
Marc-Antoine Parent <maparent@acm.org>
2016-12-16 14:47:13 -0500

    Correct CKEDITOR_BASEPATH; modernize CKEDITOR config.

--- assembl/static/js/app/views/reusableDataFields/ckeditorField.js
+++ assembl/static/js/app/views/reusableDataFields/ckeditorField.js
@@ -21,25 +21,9 @@ CKEDITOR.editorConfig = function( config ) {
   // http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
   // The toolbar groups arrangement, optimized for a single toolbar row.
-  config.toolbarGroups = [
-                          { name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
-                          { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-                          { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-                          { name: 'forms' },
-                          { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-                          { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-                          { name: 'links' },
-                          { name: 'insert' },
-                          { name: 'styles' },
-                          { name: 'colors' },
-                          { name: 'tools' },
-                          { name: 'others' },
-                          { name: 'about' }
-                          ];
-
-  // The default plugins included in the basic setup define some buttons that
-  // are not needed in a basic editor. They are removed here.
-  config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+  config.toolbar = [
+    { name: 'base', items: [ 'Bold', 'Italic', 'Outdent', 'Indent','NumberedList', 'BulletedList', 'Link', 'Unlink', 'Anchor', 'PasteText' ] },
+  ];
 
   // Dialog windows are also simplified.
   config.removeDialogTabs = 'link:advanced';
@@ -248,7 +232,7 @@ var cKEditorField = Marionette.ItemView.extend({
         */
         setTimeout(function() {
 
-          if (!that.ckInstance.element) return;
+          if (!that.ckInstance || !that.ckInstance.element) return;
 
           var hasFocus = $(that.ckInstance.element).is(":focus");
 
