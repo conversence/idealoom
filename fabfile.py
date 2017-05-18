363b77a8ded69759aab8b0223cf8bea445f5640c
Marc-Antoine Parent <maparent@acm.org>
2017-05-18 16:47:26 -0400

    redis and memcached are part of singleserver

--- fabfile.py
+++ fabfile.py
@@ -774,6 +774,8 @@ def install_single_server():
     execute(install_database)
     execute(install_assembl_server_deps)
     execute(check_and_create_database_user)
+    execute(install_redis)
+    execute(install_memcached)
 
 
 @task
@@ -784,8 +786,6 @@ def install_assembl_server_deps():
     sanitize_env()
     execute(skeleton_env, None)
     execute(install_assembl_deps)
-    execute(install_redis)
-    execute(install_memcached)
 
 
 @task
