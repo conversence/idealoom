38e7083c5d3fb6c2c0d6de8dfff9b66a82946013
Marc-Antoine Parent <maparent@acm.org>
2017-07-17 08:31:44 -0400

    pyramid_retry

--- assembl/__init__.py
+++ assembl/__init__.py
@@ -66,6 +66,7 @@ def main(global_config, **settings):
             'assembl.tweens.virtuoso_deadlock.transient_deadlock_tween_factory',
             under="pyramid_tm.tm_tween_factory")
 
+    config.include('pyramid_retry')
     config.include('.auth')
     config.include('.models')
     bake_lazy_loaders()
