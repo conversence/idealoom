kamiljaszczynski/assembl

Let the post calculate prior probabilities of different languages, and use the translation service to guess the languages based on those.

assembl/models/auth.py
@@ -1695,6 +1695,9 @@ class LanguagePreferenceCollection(object):
     def default_locale_code(self):
         pass
 
+    @abstractmethod
+    def known_languages(self):
+        return []
 
 class LanguagePreferenceCollectionWithDefault(LanguagePreferenceCollection):
     """A LanguagePreferenceCollection with a fallback language."""
@@ -1716,6 +1719,9 @@ class LanguagePreferenceCollectionWithDefault(LanguagePreferenceCollection):
                 translate_to_locale=self.default_locale,
                 translate_to=self.default_locale.id,
                 source_of_evidence=LanguagePreferenceOrder.Cookie.value)
+
+    def known_languages(self):
+        return [self.default_locale]
 
 
 class UserLanguagePreferenceCollection(LanguagePreferenceCollection):
@@ -1792,6 +1798,10 @@ class UserLanguagePreferenceCollection(LanguagePreferenceCollection):
             translate_to=self.default_pref.locale.id,
             source_of_evidence=self.default_pref.source_of_evidence,
             user=None)  # Do not give the user or this gets added to session
+
+    def known_languages(self):
+        return list({pref.translate_to_code or pref.locale_code
+                     for pref in self.user_prefs.itervalues()})
 
 
 class UserLanguagePreference(Base):

assembl/models/generic.py
@@ -541,6 +541,33 @@ class Content(TombstonableMixin, DiscussionBoundBase):
         user_id = get_current_user_id()
         return User.uri_generic(user_id)
 
+    def language_priors(self):
+        from .langstrings import Locale
+        discussion = self.discussion
+        discussion_locales = discussion.discussion_locales
+        return {Locale.extract_root_locale(loc): 1
+                for loc in discussion_locales}
+
+    def guess_languages(self):
+        from ..nlp.translation_service import TranslationService
+        if self.discussion is None:
+            self.discussion = Discussion.get(self.discussion_id)
+        assert self.discussion
+        ts = self.discussion.translation_service()
+        priors = self.language_priors()
+        if self.body:
+            body_original = self.body.first_original()
+            TranslationService.confirm_locale_c(
+                body_original, self.discussion, ts, priors)
+        if self.subject:
+            if self.body:
+                # boost the body's language
+                priors = {k: v * 0.8 for (k, v) in priors.iteritems()}
+                priors[body_original.locale_code] = 1
+            subject_original = self.subject.first_original()
+            TranslationService.confirm_locale_c(
+                subject_original, self.discussion, ts, priors)
+
     @property
     def my_sentiment(self):
         # Use only within request

assembl/models/langstrings.py
@@ -438,6 +438,31 @@ class LangString(Base):
             locale_id=Locale.get_id_of(locale_code))
         return ls
 
+    def add_value(self, value, locale_code=Locale.UNDEFINED):
+        self.add_entry(LangStringEntry(
+            langstring=self, value=value,
+            locale_id=Locale.get_id_of(locale_code)))
+
+    @classmethod
+    def create_localized_langstring(
+            cls, trans_string, desired_locales=None, known_translations=None):
+        """Construct a langstring from a localized string.
+        Call with a TranslationString."""
+        inst = cls.create(trans_string, 'en')
+        known_translations = known_translations or {}
+        for loc in desired_locales or ():
+            if loc == 'en':
+                continue
+            elif loc in known_translations:
+                inst.add_value(known_translations[loc], loc)
+            else:
+                from pyramid.i18n import make_localizer
+                from os.path import dirname, join
+                loc_dir = join(dirname(dirname(__file__)), 'locale')
+                localizer = make_localizer(loc, loc_dir)
+                inst.add_value(localizer.translate(trans_string), loc)
+        return inst
+
     @property
     def entries_as_dict(self):
         return {e.locale_id: e for e in self.entries}
@@ -912,7 +937,7 @@ class LangStringEntry(TombstonableMixin, Base):
             raise RuntimeError("Why identify a machine-translated locale?")
         data = data or {}
         original = self.locale_identification_data_json.get("original", None)
-        if not locale_code:
+        if not locale_code or locale_code == Locale.UNDEFINED:
             if not self.locale_code or self.locale_code == Locale.UNDEFINED:
                 # replace id data with new one.
                 if original:

assembl/models/post.py
@@ -520,6 +520,33 @@ class Post(Content):
             icls = self.filter_idea_content_links_r(icls)
         return icls
 
+    def language_priors(self):
+        from .auth import User, UserLanguagePreferenceCollection
+        from .langstrings import Locale
+        priors = super(Post, self).language_priors()
+        creator = self.creator or AgentProfile.get(self.creator_id)
+        if creator and isinstance(creator, User):
+            # probably a language that the user knows
+            try:
+                prefs = UserLanguagePreferenceCollection(creator.id)
+                known_languages = prefs.known_languages()
+            except AssertionError:  # user without prefs
+                from pyramid.threadlocal import get_current_request
+                request = get_current_request()
+                if request:
+                    known_languages = [request.locale_name]
+                else:
+                    return priors
+                known_languages = []
+            known_languages = {Locale.extract_root_locale(loc)
+                               for loc in known_languages}
+            priors = {k: v * (1 if k in known_languages else 0.8)
+                      for (k, v) in priors.iteritems()}
+            for lang in known_languages:
+                if lang not in priors:
+                    priors[lang] = 1
+        return priors
+
     @classmethod
     def extra_collections(cls):
         from .idea_content_link import IdeaContentLink

assembl/nlp/translation_service.py
@@ -133,20 +133,15 @@ class TranslationService(object):
             return Locale.UNDEFINED, {Locale.UNDEFINED: 1}
         len_nourl = cls.strlen_nourl(text)
         if len_nourl < 5:
-            return Locale.NON_LINGUISTIC
-        init_factory()
-        from langdetect.detector_factory import _factory as detector_factory
-        detector = detector_factory.create()
+            return Locale.NON_LINGUISTIC, {Locale.NON_LINGUISTIC: 1}
+        detector = cls.detector_factory().create()
         if constrain_locale_threshold and (
                 len_nourl < constrain_locale_threshold):
             excluded_probability = 0
         else:
             # Give less probability to excluded languages for shorter texts
             excluded_probability = min(1, log(len_nourl) / 10)
-        expected_locales = {
-            Locale.extract_root_locale(locale) for locale in expected_locales}
-        priors = {loc: 1 if loc in expected_locales else excluded_probability
-                  for loc in detector.langlist}
+        priors = cls.convert_to_priors(expected_locales, excluded_probability)
         detector.set_prior_map(priors)
         detector.append(text)
         language_data = detector.get_probabilities()
@@ -156,20 +151,38 @@ class TranslationService(object):
                              ) else Locale.UNDEFINED
         return top, {lang: prob for (prob, lang) in data}
 
+    @staticmethod
+    def detector_factory():
+        init_factory()
+        from langdetect.detector_factory import _factory as detector_factory
+        return detector_factory
+
+    @staticmethod
+    def convert_to_priors(priors, base_rate=0.1):
+        if isinstance(priors, list):
+            priors = {Locale.extract_root_locale(l): 1 for l in priors}
+        if base_rate > 0:
+            factory = TranslationService.detector_factory()
+            if len(priors) < len(factory.langlist):
+                priors0 = {l: base_rate for l in factory.langlist}
+                priors0.update(priors)
+                priors = priors0
+        return priors
 
     def confirm_locale(
-            self, langstring_entry,
+            self, langstring_entry, priors=None,
             constrain_locale_threshold=SECURE_IDENTIFICATION_LIMIT):
         return self.confirm_locale_c(
-            langstring_entry, discussion=self.discussion, ts=self,
+            langstring_entry, discussion=self.discussion,
+            ts=self, priors=priors,
             constrain_locale_threshold=constrain_locale_threshold)
 
     @classmethod
-    def confirm_locale_c(cls, langstring_entry, discussion, ts=None,
+    def confirm_locale_c(cls, langstring_entry, discussion, ts=None, priors=None,
                          constrain_locale_threshold=SECURE_IDENTIFICATION_LIMIT):
         try:
             ts = ts or discussion.translation_service()
-            expected_locales = discussion.discussion_locales
+            expected_locales = priors or discussion.discussion_locales
             if ts:
                 lang, data = ts.identify(langstring_entry.value, expected_locales)
                 data["service"] = ts.__class__.__name__
@@ -512,7 +525,9 @@ class GoogleTranslationService(DummyGoogleTranslationService):
                 text, expected_locales, constrain_locale_threshold))
         r = self.client.detections().list(q=text).execute()
         r = r[u"detections"][0]
-        r.sort(lambda x: x[u"confidence"], reverse=True)
+        # small correction for expected languages, as this service is deemed reliable.
+        priors = self.convert_to_priors(expected_locales, 0.8)
+        r.sort(lambda x: x[u"confidence"] * priors.get(x[u'language'], 0.8), reverse=True)
         # Not sure about how to interpret isReliable,
         # it seems to always be false.
         return self.asPosixLocale(r[0][u"language"]), {

conversence/idealoom

assembl/models/auth.py
@@ -1713,6 +1713,9 @@ class LanguagePreferenceCollection(object):
     def default_locale_code(self):
         pass
 
+    @abstractmethod
+    def known_languages(self):
+        return []
 
 class LanguagePreferenceCollectionWithDefault(LanguagePreferenceCollection):
     """A LanguagePreferenceCollection with a fallback language."""
@@ -1732,6 +1735,9 @@ class LanguagePreferenceCollectionWithDefault(LanguagePreferenceCollection):
                 locale=locale,
                 translate=self.default_locale,
                 source_of_evidence=LanguagePreferenceOrder.Cookie.value)
+
+    def known_languages(self):
+        return [self.default_locale]
 
 
 class UserLanguagePreferenceCollection(LanguagePreferenceCollection):
@@ -1806,6 +1812,10 @@ class UserLanguagePreferenceCollection(LanguagePreferenceCollection):
             translate=self.default_pref.locale,
             source_of_evidence=self.default_pref.source_of_evidence,
             user=None)  # Do not give the user or this gets added to session
+
+    def known_languages(self):
+        return list({pref.translate or pref.locale
+                     for pref in self.user_prefs.values()})
 
 
 @total_ordering

assembl/models/generic.py
@@ -518,6 +518,33 @@ class Content(TombstonableOriginMixin, DiscussionBoundBase):
                 name=QUADNAMES.post_external_link_map)
         ]
 
+    def language_priors(self):
+        from .langstrings import Locale
+        discussion = self.discussion
+        discussion_locales = discussion.discussion_locales
+        return {Locale.extract_root_locale(loc): 1
+                for loc in discussion_locales}
+
+    def guess_languages(self):
+        from ..nlp.translation_service import TranslationService
+        if self.discussion is None:
+            self.discussion = Discussion.get(self.discussion_id)
+        assert self.discussion
+        ts = self.discussion.translation_service()
+        priors = self.language_priors()
+        if self.body:
+            body_original = self.body.first_original()
+            TranslationService.confirm_locale_c(
+                body_original, self.discussion, ts, priors)
+        if self.subject:
+            if self.body:
+                # boost the body's language
+                priors = {k: v * 0.8 for (k, v) in priors.items()}
+                priors[body_original.locale_code] = 1
+            subject_original = self.subject.first_original()
+            TranslationService.confirm_locale_c(
+                subject_original, self.discussion, ts, priors)
+
     widget_idea_links = relationship('IdeaContentWidgetLink')
 
     def indirect_idea_content_links(self):

assembl/models/langstrings.py
@@ -154,6 +154,31 @@ class LangString(Base):
             locale=locale_code)
         return ls
 
+    def add_value(self, value, locale_code=Locale.UNDEFINED):
+        self.add_entry(LangStringEntry(
+            langstring=self, value=value,
+            locale_id=Locale.get_id_of(locale_code)))
+
+    @classmethod
+    def create_localized_langstring(
+            cls, trans_string, desired_locales=None, known_translations=None):
+        """Construct a langstring from a localized string.
+        Call with a TranslationString."""
+        inst = cls.create(trans_string, 'en')
+        known_translations = known_translations or {}
+        for loc in desired_locales or ():
+            if loc == 'en':
+                continue
+            elif loc in known_translations:
+                inst.add_value(known_translations[loc], loc)
+            else:
+                from pyramid.i18n import make_localizer
+                from os.path import dirname, join
+                loc_dir = join(dirname(dirname(__file__)), 'locale')
+                localizer = make_localizer(loc, loc_dir)
+                inst.add_value(localizer.translate(trans_string), loc)
+        return inst
+
     @property
     def entries_as_dict(self):
         return {e.locale: e for e in self.entries}
@@ -577,7 +602,7 @@ class LangStringEntry(TombstonableMixin, Base):
             raise RuntimeError("Why identify a machine-translated locale?")
         data = data or {}
         original = self.locale_identification_data_json.get("original", None)
-        if not locale_code:
+        if not locale_code or locale_code == LocaleLabel.UNDEFINED:
             if not self.locale or self.locale == LocaleLabel.UNDEFINED:
                 # replace id data with new one.
                 if original:

assembl/models/post.py
@@ -529,6 +529,33 @@ class Post(Content):
             icls = self.filter_idea_content_links_r(icls)
         return icls
 
+    def language_priors(self):
+        from .auth import User, UserLanguagePreferenceCollection
+        from .langstrings import Locale
+        priors = super(Post, self).language_priors()
+        creator = self.creator or AgentProfile.get(self.creator_id)
+        if creator and isinstance(creator, User):
+            # probably a language that the user knows
+            try:
+                prefs = UserLanguagePreferenceCollection(creator.id)
+                known_languages = prefs.known_languages()
+            except AssertionError:  # user without prefs
+                from pyramid.threadlocal import get_current_request
+                request = get_current_request()
+                if request:
+                    known_languages = [request.locale_name]
+                else:
+                    return priors
+                known_languages = []
+            known_languages = {Locale.extract_root_locale(loc)
+                               for loc in known_languages}
+            priors = {k: v * (1 if k in known_languages else 0.8)
+                      for (k, v) in priors.items()}
+            for lang in known_languages:
+                if lang not in priors:
+                    priors[lang] = 1
+        return priors
+
     @classmethod
     def extra_collections(cls):
         from .idea_content_link import IdeaContentLink

assembl/nlp/translation_service.py
@@ -138,20 +138,15 @@ class TranslationService(object):
             return LocaleLabel.UNDEFINED, {LocaleLabel.UNDEFINED: 1}
         len_nourl = cls.strlen_nourl(text)
         if len_nourl < 5:
-            return LocaleLabel.NON_LINGUISTIC
-        init_factory()
-        from langdetect.detector_factory import _factory as detector_factory
-        detector = detector_factory.create()
+            return LocaleLabel.NON_LINGUISTIC, {LocaleLabel.NON_LINGUISTIC: 1}
+        detector = cls.detector_factory().create()
         if constrain_locale_threshold and (
                 len_nourl < constrain_locale_threshold):
             excluded_probability = 0
         else:
             # Give less probability to excluded languages for shorter texts
             excluded_probability = min(1, log(len_nourl) / 10)
-        expected_locales = {
-            Locale.extract_root_locale(locale) for locale in expected_locales}
-        priors = {loc: 1 if loc in expected_locales else excluded_probability
-                  for loc in detector.langlist}
+        priors = cls.convert_to_priors(expected_locales, excluded_probability)
         detector.set_prior_map(priors)
         detector.append(text)
         language_data = detector.get_probabilities()
@@ -161,20 +156,38 @@ class TranslationService(object):
                              ) else LocaleLabel.UNDEFINED
         return top, {lang: prob for (prob, lang) in data}
 
+    @staticmethod
+    def detector_factory():
+        init_factory()
+        from langdetect.detector_factory import _factory as detector_factory
+        return detector_factory
+
+    @staticmethod
+    def convert_to_priors(priors, base_rate=0.1):
+        if isinstance(priors, list):
+            priors = {Locale.extract_root_locale(l): 1 for l in priors}
+        if base_rate > 0:
+            factory = TranslationService.detector_factory()
+            if len(priors) < len(factory.langlist):
+                priors0 = {l: base_rate for l in factory.langlist}
+                priors0.update(priors)
+                priors = priors0
+        return priors
 
     def confirm_locale(
-            self, langstring_entry,
+            self, langstring_entry, priors=None,
             constrain_locale_threshold=SECURE_IDENTIFICATION_LIMIT):
         return self.confirm_locale_c(
-            langstring_entry, discussion=self.discussion, ts=self,
+            langstring_entry, discussion=self.discussion,
+            ts=self, priors=priors,
             constrain_locale_threshold=constrain_locale_threshold)
 
     @classmethod
-    def confirm_locale_c(cls, langstring_entry, discussion, ts=None,
+    def confirm_locale_c(cls, langstring_entry, discussion, ts=None, priors=None,
                          constrain_locale_threshold=SECURE_IDENTIFICATION_LIMIT):
         try:
             ts = ts or discussion.translation_service()
-            expected_locales = discussion.discussion_locales
+            expected_locales = priors or discussion.discussion_locales
             if ts:
                 lang, data = ts.identify(langstring_entry.value, expected_locales)
                 data["service"] = ts.__class__.__name__
@@ -508,7 +521,9 @@ class GoogleTranslationService(DummyGoogleTranslationService):
                 text, expected_locales, constrain_locale_threshold))
         r = self.client.detections().list(q=text).execute()
         r = r[u"detections"][0]
-        r.sort(lambda x: x[u"confidence"], reverse=True)
+        # small correction for expected languages, as this service is deemed reliable.
+        priors = self.convert_to_priors(expected_locales, 0.8)
+        r.sort(lambda x: x[u"confidence"] * priors.get(x[u'language'], 0.8), reverse=True)
         # Not sure about how to interpret isReliable,
         # it seems to always be false.
         return self.asPosixLocale(r[0][u"language"]), {