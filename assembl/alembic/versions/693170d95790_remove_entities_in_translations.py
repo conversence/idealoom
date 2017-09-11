bfcfa9cd135c0b052b0933fc748a7cb91dc729b3
Marc-Antoine Parent <maparent@acm.org>
2017-09-11 09:39:50 -0400

    remove entities in existing translations

---
+++ assembl/alembic/versions/693170d95790_remove_entities_in_translations.py
@@ -0,0 +1,42 @@
+"""remove entities in translations
+
+Revision ID: 693170d95790
+Revises: a33ea73a4b2e
+Create Date: 2017-09-11 08:40:32.676764
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '693170d95790'
+down_revision = 'a33ea73a4b2e'
+
+from alembic import context, op
+import sqlalchemy as sa
+from sqlalchemy.orm import aliased
+import transaction
+
+
+from assembl.lib import config
+
+
+def upgrade(pyramid_env):
+    # Do stuff with the app's models here.
+    from assembl import models as m
+    from assembl.lib.clean_input import unescape
+    db = m.get_session_maker()()
+    with transaction.manager:
+        lse = aliased(m.LangStringEntry)
+        q = db.query(m.Content).join(lse, lse.langstring_id==m.Content.body_id
+            ).filter(lse.mt_trans_of_id != None, lse.value != None
+            ).add_entity(lse)
+        for content, entry in q:
+            if content.get_body_mime_type() == 'text/plain':
+                entry.value = unescape(entry.value)
+        q = db.query(lse).join(m.Content, lse.langstring_id==m.Content.subject_id
+            ).filter(lse.mt_trans_of_id != None, lse.value != None)
+        for entry in q:
+            entry.value = unescape(entry.value)
+
+
+def downgrade(pyramid_env):
+    pass
