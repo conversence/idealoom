assembl/assembl

Record when a user was logged in with a given account. Use this to know when the user needs to login, according to this account's expiry requirements.

assembl/alembic/versions/8ed4db9d977f_account_last_checked.py
@@ -1,0 +1,47 @@
+"""account last_checked
+
+Revision ID: 8ed4db9d977f
+Revises: 27f58fce7b77
+Create Date: 2017-11-22 08:45:22.805166
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '8ed4db9d977f'
+down_revision = '27f58fce7b77'
+
+from alembic import context, op
+import sqlalchemy as sa
+
+
+def upgrade(pyramid_env):
+    with context.begin_transaction():
+        op.add_column(
+            "user",
+            sa.Column("last_assembl_login", sa.DateTime))
+        op.add_column(
+            "social_auth_account",
+            sa.Column("last_checked", sa.DateTime))
+        op.execute('UPDATE public.user SET last_assembl_login = last_login')
+        # set last_checked from last_login when single account
+        op.execute('''
+            UPDATE social_auth_account SET last_checked = (
+                SELECT last_login FROM public.user
+                    JOIN abstract_agent_account ON public.user.id = abstract_agent_account.profile_id
+                    WHERE abstract_agent_account.id = social_auth_account.id)
+            WHERE id IN (
+                SELECT min(abstract_agent_account.id)
+                FROM abstract_agent_account
+                JOIN public.user ON (public.user.id = abstract_agent_account.profile_id)
+                WHERE abstract_agent_account.verified
+                GROUP BY profile_id
+                HAVING count(abstract_agent_account.id) = 1
+                    AND min(abstract_agent_account.type)='social_auth_account')''')
+
+
+def downgrade(pyramid_env):
+    with context.begin_transaction():
+        op.drop_column(
+            "social_auth_account", "last_checked")
+        op.drop_column(
+            "user", "last_assembl_login")

assembl/models/auth.py
@@ -1,5 +1,5 @@
 """All classes relative to users and their online identities."""
-from datetime import datetime
+from datetime import datetime, timedelta
 from itertools import chain, permutations
 import urllib
 import hashlib
@@ -356,6 +356,55 @@ class AgentProfile(Base):
             return prefs[0]
         return Locale.locale_collection_byid[prefs[0].locale_id]
 
+    def successful_login(self, social=False):
+        "A successful email login"
+        self.last_login = datetime.utcnow()
+        if not social:
+            self.last_assembl_login = self.last_login
+
+    def assembl_login_expiry(self):
+        duration = config.get('login_expiry_email', None)
+        if duration is None:
+            # default to no expiry
+            duration = config.get('login_expiry_default', 0)
+        if not duration:
+            return None
+        last_login = self.last_assembl_login
+        if not last_login:
+            return datetime.utcnow() - timedelta(seconds=1)
+        return last_login + timedelta(float(duration))
+
+    def login_expiry(self, discussion):
+        accounts = [a for a in self.social_accounts if a.verified]
+        autologin = None
+        if discussion:
+            autologin = discussion.preferences['authorization_server_backend']
+        from ..auth.util import user_has_permission
+        if autologin and not user_has_permission(
+                discussion.id, self.id, P_OVERRIDE_SOCIAL_AUTOLOGIN):
+            # the discussion restricts access to this specific
+            # social identity provider. The override permission
+            # bypasses that, mostly for external moderators.
+            autologin_accs_expiry = [
+                a.login_expiry() for a in accounts
+                if a.provider_with_idp == autologin]
+            if len(autologin_accs_expiry):
+                if None in autologin_accs_expiry:
+                    return None
+                return max(autologin_accs_expiry)
+            return datetime.utcnow() - timedelta(seconds=1)
+        expiries = [a.login_expiry() for a in accounts]
+        expiries.append(self.assembl_login_expiry())
+        if None in expiries:
+            return None
+        return max(expiries)
+
+    def login_expired(self, discussion):
+        expiry = self.login_expiry(discussion)
+        if expiry is None:
+            return False
+        return expiry < datetime.utcnow()
+
 
 class AbstractAgentAccount(Base):
     """An abstract class for online accounts that identify AgentsProfiles
@@ -643,10 +692,12 @@ class User(AgentProfile):
     password = deferred(Column(Binary(115)))
     timezone = Column(Time(True))
     last_login = Column(DateTime)
+    last_assembl_login = Column(DateTime)
     login_failures = Column(Integer, default=0)
     creation_date = Column(DateTime, nullable=False, default=datetime.utcnow,
         info={'rdf': QuadMapPatternS(
             None, DCTERMS.created, sections=(PRIVATE_USER_SECTION,))})
+    social_accounts = relationship('SocialAuthAccount')
 
     def __init__(self, **kwargs):
         if kwargs.get('password', None) is not None:

conversence/idealoom

assembl/alembic/versions/8ed4db9d977f_account_last_checked.py
@@ -1,0 +1,47 @@
+"""account last_checked
+
+Revision ID: 8ed4db9d977f
+Revises: 27f58fce7b77
+Create Date: 2017-11-22 08:45:22.805166
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '8ed4db9d977f'
+down_revision = '27f58fce7b77'
+
+from alembic import context, op
+import sqlalchemy as sa
+
+
+def upgrade(pyramid_env):
+    with context.begin_transaction():
+        op.add_column(
+            "user",
+            sa.Column("last_idealoom_login", sa.DateTime))
+        op.add_column(
+            "social_auth_account",
+            sa.Column("last_checked", sa.DateTime))
+        op.execute('UPDATE public.user SET last_idealoom_login = last_login')
+        # set last_checked from last_login when single account
+        op.execute('''
+            UPDATE social_auth_account SET last_checked = (
+                SELECT last_login FROM public.user
+                    JOIN abstract_agent_account ON public.user.id = abstract_agent_account.profile_id
+                    WHERE abstract_agent_account.id = social_auth_account.id)
+            WHERE id IN (
+                SELECT min(abstract_agent_account.id)
+                FROM abstract_agent_account
+                JOIN public.user ON (public.user.id = abstract_agent_account.profile_id)
+                WHERE abstract_agent_account.verified
+                GROUP BY profile_id
+                HAVING count(abstract_agent_account.id) = 1
+                    AND min(abstract_agent_account.type)='social_auth_account')''')
+
+
+def downgrade(pyramid_env):
+    with context.begin_transaction():
+        op.drop_column(
+            "social_auth_account", "last_checked")
+        op.drop_column(
+            "user", "last_idealoom_login")

assembl/models/auth.py
@@ -3,7 +3,7 @@ from future import standard_library
 standard_library.install_aliases()
 from builtins import str
 from builtins import object
-from datetime import datetime
+from datetime import datetime, timedelta
 from itertools import chain, permutations
 from functools import total_ordering
 import urllib.request, urllib.parse, urllib.error
@@ -372,6 +372,55 @@ class AgentProfile(Base):
             return prefs[0]
         return prefs[0].locale
 
+    def successful_login(self, social=False):
+        "A successful email login"
+        self.last_login = datetime.utcnow()
+        if not social:
+            self.last_idealoom_login = self.last_login
+
+    def assembl_login_expiry(self):
+        duration = config.get('login_expiry_email', None)
+        if duration is None:
+            # default to no expiry
+            duration = config.get('login_expiry_default', 0)
+        if not duration:
+            return None
+        last_login = self.last_idealoom_login
+        if not last_login:
+            return datetime.utcnow() - timedelta(seconds=1)
+        return last_login + timedelta(float(duration))
+
+    def login_expiry(self, discussion):
+        accounts = [a for a in self.social_accounts if a.verified]
+        autologin = None
+        if discussion:
+            autologin = discussion.preferences['authorization_server_backend']
+        from ..auth.util import user_has_permission
+        if autologin and not user_has_permission(
+                discussion.id, self.id, P_OVERRIDE_SOCIAL_AUTOLOGIN):
+            # the discussion restricts access to this specific
+            # social identity provider. The override permission
+            # bypasses that, mostly for external moderators.
+            autologin_accs_expiry = [
+                a.login_expiry() for a in accounts
+                if a.provider_with_idp == autologin]
+            if len(autologin_accs_expiry):
+                if None in autologin_accs_expiry:
+                    return None
+                return max(autologin_accs_expiry)
+            return datetime.utcnow() - timedelta(seconds=1)
+        expiries = [a.login_expiry() for a in accounts]
+        expiries.append(self.assembl_login_expiry())
+        if None in expiries:
+            return None
+        return max(expiries)
+
+    def login_expired(self, discussion):
+        expiry = self.login_expiry(discussion)
+        if expiry is None:
+            return False
+        return expiry < datetime.utcnow()
+
 
 class AbstractAgentAccount(Base):
     """An abstract class for online accounts that identify AgentsProfiles
@@ -670,8 +719,10 @@ class User(NamedClassMixin, OriginMixin, AgentProfile):
     password = deferred(Column(Binary(115)))
     timezone = Column(Time(True))
     last_login = Column(DateTime)
+    last_idealoom_login = Column(DateTime)
     login_failures = Column(Integer, default=0)
     username = Column(CoerceUnicode(20), unique=True)
+    social_accounts = relationship('SocialAuthAccount')
 
     def __init__(self, **kwargs):
         if kwargs.get('password', None) is not None: