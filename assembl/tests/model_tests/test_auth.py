assembl/assembl

test login expiry logic.

assembl/tests/model_tests/test_auth.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 
 from assembl.models.notification import (
     ModelEventWatcherNotificationSubscriptionDispatcher)
@@ -17,3 +18,54 @@ def test_subscribe_to_discussion(
     participant2_user.unsubscribe(discussion)
     test_session.flush()
     assert discussion in participant2_user.participant_in_discussion, "The user should no longer be subscribed to the discussion"
+
+
+def test_general_expiry(
+        test_session, participant1_user, participant1_social_account, discussion):
+    long_ago = datetime(2000, 1, 1)
+    now = datetime.utcnow()
+    # if all logins are old, our login is expired
+    participant1_user.last_assembl_login = long_ago
+    participant1_social_account.last_checked = long_ago
+    assert participant1_user.login_expired(discussion)
+    # if either social or assembl login is recent, our login is valid
+    participant1_user.last_assembl_login = now
+    assert not participant1_user.login_expired(discussion)
+    participant1_user.last_assembl_login = long_ago
+    participant1_social_account.last_checked = now
+    assert not participant1_user.login_expired(discussion)
+
+
+def test_restricted_discussion_expiry(
+        test_session, participant1_user, participant1_social_account,
+        closed_discussion):
+    long_ago = datetime(2000, 1, 1)
+    now = datetime.utcnow()
+    # if our logins are old, our login is still expired
+    participant1_user.last_assembl_login = long_ago
+    participant1_social_account.last_checked = long_ago
+    assert participant1_user.login_expired(closed_discussion)
+    # if our last login was through assembl, no change
+    participant1_user.last_assembl_login = now
+    assert participant1_user.login_expired(closed_discussion)
+    # only appropriate social login counts
+    participant1_user.last_assembl_login = long_ago
+    participant1_social_account.last_checked = now
+    assert not participant1_user.login_expired(closed_discussion)
+
+
+def test_restricted_discussion_expiry_override(
+        test_session, admin_user, admin_social_account, closed_discussion):
+    long_ago = datetime(2000, 1, 1)
+    now = datetime.utcnow()
+    # if our logins are old, our login is still expired
+    admin_user.last_assembl_login = long_ago
+    admin_social_account.last_checked = long_ago
+    assert admin_user.login_expired(closed_discussion)
+    # if our last login was through assembl, works because override
+    admin_user.last_assembl_login = now
+    assert not admin_user.login_expired(closed_discussion)
+    # appropriate social login still counts
+    admin_user.last_assembl_login = long_ago
+    admin_social_account.last_checked = now
+    assert not admin_user.login_expired(closed_discussion)

conversence/idealoom

assembl/tests/model_tests/test_auth.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 
 from assembl.models.notification import (
     ModelEventWatcherNotificationSubscriptionDispatcher)
@@ -17,3 +18,54 @@ def test_subscribe_to_discussion(
     participant2_user.unsubscribe(discussion)
     test_session.flush()
     assert discussion in participant2_user.participant_in_discussion, "The user should no longer be subscribed to the discussion"
+
+
+def test_general_expiry(
+        test_session, participant1_user, participant1_social_account, discussion):
+    long_ago = datetime(2000, 1, 1)
+    now = datetime.utcnow()
+    # if all logins are old, our login is expired
+    participant1_user.last_idealoom_login = long_ago
+    participant1_social_account.last_checked = long_ago
+    assert participant1_user.login_expired(discussion)
+    # if either social or assembl login is recent, our login is valid
+    participant1_user.last_idealoom_login = now
+    assert not participant1_user.login_expired(discussion)
+    participant1_user.last_idealoom_login = long_ago
+    participant1_social_account.last_checked = now
+    assert not participant1_user.login_expired(discussion)
+
+
+def test_restricted_discussion_expiry(
+        test_session, participant1_user, participant1_social_account,
+        closed_discussion):
+    long_ago = datetime(2000, 1, 1)
+    now = datetime.utcnow()
+    # if our logins are old, our login is still expired
+    participant1_user.last_idealoom_login = long_ago
+    participant1_social_account.last_checked = long_ago
+    assert participant1_user.login_expired(closed_discussion)
+    # if our last login was through assembl, no change
+    participant1_user.last_idealoom_login = now
+    assert participant1_user.login_expired(closed_discussion)
+    # only appropriate social login counts
+    participant1_user.last_idealoom_login = long_ago
+    participant1_social_account.last_checked = now
+    assert not participant1_user.login_expired(closed_discussion)
+
+
+def test_restricted_discussion_expiry_override(
+        test_session, admin_user, admin_social_account, closed_discussion):
+    long_ago = datetime(2000, 1, 1)
+    now = datetime.utcnow()
+    # if our logins are old, our login is still expired
+    admin_user.last_idealoom_login = long_ago
+    admin_social_account.last_checked = long_ago
+    assert admin_user.login_expired(closed_discussion)
+    # if our last login was through assembl, works because override
+    admin_user.last_idealoom_login = now
+    assert not admin_user.login_expired(closed_discussion)
+    # appropriate social login still counts
+    admin_user.last_idealoom_login = long_ago
+    admin_social_account.last_checked = now
+    assert not admin_user.login_expired(closed_discussion)