e12107e00e8bad75eef4b2e3900c05277786a831
Marc-Antoine Parent <maparent@acm.org>
2018-02-20 05:21:02 -0500

    allow proper get with default in preferences. defensive coding in social_auth, until that function is removed

--- assembl/models/preferences.py
+++ assembl/models/preferences.py
@@ -109,7 +109,7 @@ class Preferences(MutableMapping, NamedClassMixin, AbstractBase):
 
     def _get_local(self, key):
         if key not in self.preference_data:
-            raise RuntimeError("Unknown preference: " + key)
+            raise KeyError("Unknown preference: " + key)
         values = self.local_values_json
         if key in values:
             value = values[key]
