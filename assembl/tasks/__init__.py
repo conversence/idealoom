60416ff6f4e992fbb65d597bc42fe782c6594c76
Marc-Antoine Parent <maparent@acm.org>
2018-08-05 16:25:44 -0400

    single celery wip

--- assembl/tasks/__init__.py
+++ assembl/tasks/__init__.py
@@ -18,8 +18,9 @@ import logging
 
 from pyramid.paster import get_appsettings
 from pyramid.path import DottedNameResolver
-from kombu import Exchange, Queue
+from datetime import timedelta
 from celery import Celery
+from pyramid_mailer import mailer_factory_from_settings
 
 from ..lib.sqla import configure_engine
 from ..lib.zmqlib import configure_zmq
@@ -27,6 +28,8 @@ from ..lib.config import set_config
 from assembl.lib.raven_client import setup_raven, capture_exception
 from zope.component import getGlobalSiteManager
 from ..lib.model_watcher import configure_model_watcher
+from ..lib.logging import getLogger
+
 
 _settings = None
 log = logging.getLogger(__name__)
@@ -38,116 +41,96 @@ def configure(registry, task_name):
     settings = registry.settings
     if _settings is None:
         _settings = settings
-    if settings.get('%s_debug_signal' % (task_name,), False):
-        from assembl.lib import signals
-        signals.listen()
-    configure_zmq(settings['changes.socket'], False)
     # temporary solution
     configure_model_watcher(registry, task_name)
-    # configure them all...
-    from .notify import notify_celery_app
-    config_celery_app(notify_celery_app, settings)
-    from .notification_dispatch import notif_dispatch_celery_app
-    config_celery_app(notif_dispatch_celery_app, settings)
-    from .translate import translation_celery_app
-    config_celery_app(translation_celery_app, settings)
-    from .imap import imap_celery_app
-    config_celery_app(imap_celery_app, settings)
-
-
-_celery_queues = None
-_celery_routes = None
-
-ASSEMBL_CELERY_APPS = {
-    'imap': 'imap_celery_app',
-    'notification_dispatch': 'notif_dispatch_celery_app',
-    'notify': 'notify_celery_app',
-    'translate': 'translation_celery_app'
-}
-
-
-def get_celery_queues():
-    global _celery_queues
-    if not _celery_queues:
-        _celery_queues = [
-            Queue(q, Exchange(q), routing_key=q)
-            for q in ASSEMBL_CELERY_APPS]
-    return _celery_queues
-
-
-def get_celery_routes():
-    global _celery_routes
-    if not _celery_routes:
-        _celery_routes = {}
-        for module, app in ASSEMBL_CELERY_APPS.items():
-            full_module_name = "assembl.tasks."+module
-            mod = __import__(full_module_name, fromlist=[app])
-            app = getattr(mod, app)
-            for task_name in app.tasks:
-                if task_name.startswith(full_module_name):
-                    _celery_routes[task_name] = {
-                        'queue': module, 'routing_key': module}
-    return _celery_routes
-
-
-def config_celery_app(celery_app, settings=None):
     config = {
-        "CELERY_QUEUES": get_celery_queues(),
-        "CELERY_ROUTES": get_celery_routes(),
         "CELERY_TASK_SERIALIZER": 'json',
         "CELERY_ACKS_LATE": True,
         "CELERY_CACHE_BACKEND": settings.get('celery_tasks.broker', ''),
         "CELERY_RESULT_BACKEND": settings.get('celery_tasks.broker', ''),
         "CELERY_STORE_ERRORS_EVEN_IF_IGNORED": True,
     }
-    if settings is not None:
-        broker = settings.get(
-            '%s.broker' % (celery_app.main,), None
-            ) or settings.get('celery_tasks.broker')
-        config['BROKER_URL'] = broker
-        config['CELERY_CACHE_BACKEND'] = broker
-        config['CELERY_RESULT_BACKEND'] = broker
-        celery_app.config_from_object(config, force=True)
-    else:
-        log.error("**** config_celery_app w/o broker. should not happen anymore")
-        celery_app.config_from_object(config)
+    config['BROKER_URL'] = settings.get(
+        '%s.broker' % (celery.main,), None
+    ) or settings.get('celery_tasks.broker')
+    celery.config_from_object(config, force=True)
+
+
+CELERYBEAT_SCHEDULE = {
+    'resend-every-10-minutes': {
+        'task': 'assembl.tasks.notify.process_pending_notifications',
+        'schedule': timedelta(seconds=600),
+        'options': {
+            'routing_key': 'notify',
+            'exchange': 'notify'
+        }
+    },
+}
+
+# Minimum delay between emails sent to a domain.
+# For this to work, you need to have a SINGLE celery process for notification.
+SMTP_DOMAIN_DELAYS = {
+    '': timedelta(0)
+}
+
+# INI file values with this prefix will be used to populate SMTP_DOMAIN_DELAYS.
+# Anything after the last dot is a domain name (including empty).
+# Use seconds (float) as values.
+SETTINGS_SMTP_DELAY = "celery_tasks.notify.smtp_delay."
 
 
 class CeleryWithConfig(Celery):
     "A Celery task that can receive settings"
 
-    def on_configure_with_settings(self, settings):
-        # Note that this can be called twice:
-        # make sure it is idempotent
-        pass
+    _preconf = {
+        "CELERYBEAT_SCHEDULE": CELERYBEAT_SCHEDULE
+    }
 
     def on_configure(self):
         global _settings
         if _settings is None:
-            init_from_celery(self)
-        self.on_configure_with_settings(_settings)
+            # i.e. includeme not called, i.e. not from pyramid
+            self.init_from_celery()
+
+    def init_from_celery(celery_app):
+        # A task is called through celery, so it may not have basic
+        # configuration setup. Go through that setup the first time.
+        global _settings
+        rootdir = dirname(dirname(dirname(realpath(__file__))))
+        settings_file = join(rootdir, 'local.ini')
+        if not exists(settings_file):
+            settings_file = join(rootdir, 'production.ini')
+        _settings = settings = get_appsettings(settings_file, 'assembl')
+        registry = getGlobalSiteManager()
+        registry.settings = settings
+        setup_raven(settings, settings_file)
+        set_config(settings)
+        configure_engine(settings, True)
+        configure(registry, celery_app.main)
+        from .threaded_model_watcher import ThreadDispatcher
+        threaded_watcher_class_name = settings.get(
+            '%s.threadedmodelwatcher' % (celery_app.main,),
+            "assembl.lib.model_watcher.BaseModelEventWatcher")
+        ThreadDispatcher.mw_class = resolver.resolve(
+            threaded_watcher_class_name)
+        self.mailer = mailer_factory_from_settings(settings)
+        # setup SETTINGS_SMTP_DELAY
+        for name, val in settings.iteritems():
+            if name.startswith(SETTINGS_SMTP_DELAY):
+                try:
+                    val = timedelta(seconds=float(val))
+                except ValueError:
+                    log.error("Not a valid value for %s: %s" % (name, val))
+                    continue
+                SMTP_DOMAIN_DELAYS[name[len(SETTINGS_SMTP_DELAY):]] = val
+        getLogger().info("SMTP_DOMAIN_DELAYS", delays=SMTP_DOMAIN_DELAYS)
+        import assembl.tasks.imap
+        import assembl.tasks.notify
+        import assembl.tasks.notification_dispatch
+        import assembl.tasks.translate
 
 
-def init_from_celery(celery_app):
-    # A task is called through celery, so it may not have basic
-    # configuration setup. Go through that setup the first time.
-    global _settings
-    rootdir = dirname(dirname(dirname(realpath(__file__))))
-    settings_file = join(rootdir, 'local.ini')
-    if not exists(settings_file):
-        settings_file = join(rootdir, 'production.ini')
-    _settings = settings = get_appsettings(settings_file, 'assembl')
-    registry = getGlobalSiteManager()
-    registry.settings = settings
-    setup_raven(settings, settings_file)
-    set_config(settings)
-    configure_engine(settings, True)
-    configure(registry, celery_app.main)
-    from .threaded_model_watcher import ThreadDispatcher
-    threaded_watcher_class_name = settings.get(
-        '%s.threadedmodelwatcher' % (celery_app.main,),
-        "assembl.lib.model_watcher.BaseModelEventWatcher")
-    ThreadDispatcher.mw_class = resolver.resolve(threaded_watcher_class_name)
+celery = CeleryWithConfig('celery_tasks')
 
 
 def includeme(config):
@@ -155,8 +138,4 @@ def includeme(config):
     _settings = config.registry.settings
     config.include('.threaded_model_watcher')
     configure(config.registry, 'assembl')
-    config.include('.imap')
-    config.include('.notification_dispatch')
-    config.include('.notify')
-    config.include('.translate')
     config.include('.source_reader')
