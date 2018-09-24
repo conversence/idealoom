oliverclock/assembl

vote result csv, first draft

assembl/views/api2/votes.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from cStringIO import StringIO
+import csv
 
 from pyramid.view import view_config
 from pyramid.httpexceptions import (
@@ -7,13 +8,16 @@ from pyramid.httpexceptions import (
 from pyramid.response import Response
 from pyramid.settings import asbool
 from simplejson import dumps
+from sqlalchemy.sql import func
+from sqlalchemy.orm import aliased
 
 from ..traversal import (CollectionContext, InstanceContext)
 from assembl.auth import (
-    P_READ, Everyone, CrudPermissions, P_ADMIN_DISC, P_VOTE)
+    P_READ, Everyone, CrudPermissions, P_ADMIN_DISC, P_VOTE, P_DISC_STATS)
 from assembl.auth.util import get_permissions
 from assembl.models import (
-    AbstractIdeaVote, User, AbstractVoteSpecification, VotingWidget)
+    Idea, AbstractIdeaVote, User, AbstractVoteSpecification, VotingWidget,
+    TokenVoteSpecification, LanguagePreferenceCollection)
 from assembl.lib.sqla import get_named_class
 from . import (FORM_HEADER, JSON_HEADER, check_permissions)
 
@@ -172,7 +176,7 @@ def vote_results(request):
 
 @view_config(context=InstanceContext, request_method='GET',
              ctx_instance_class=AbstractVoteSpecification,
-             name="vote_results_csv", permission=P_READ)
+             name="vote_results_csv", permission=P_DISC_STATS)
 def vote_results_csv(request):
     ctx = request.context
     user_id = request.authenticated_userid
@@ -194,5 +198,58 @@ def vote_results_csv(request):
             raise HTTPUnauthorized()
     output = StringIO()
     ctx._instance.csv_results(output, histogram)
+    output.seek(0)
+    return Response(body_file=output, content_type='text/csv')
+
+
+@view_config(context=InstanceContext, request_method='GET',
+             ctx_instance_class=VotingWidget,
+             name="vote_results_csv", permission=P_DISC_STATS)
+def global_vote_results_csv(request):
+    ctx = request.context
+    user_id = request.authenticated_userid
+    if not user_id:
+        raise HTTPUnauthorized
+    widget = ctx._instance
+    if widget.activity_state != "ended":
+        permissions = get_permissions(user_id, ctx.get_discussion_id())
+        if P_ADMIN_DISC not in permissions:
+            raise HTTPUnauthorized()
+    user_prefs = LanguagePreferenceCollection.getCurrent()
+    ideas = widget.db.query(Idea
+        ).join(AbstractIdeaVote, AbstractIdeaVote.idea_id==Idea.id
+        ).join(AbstractVoteSpecification
+        ).filter(AbstractVoteSpecification.widget_id==widget.id
+        ).distinct().all()
+    idea_ids = [i.id for i in ideas]
+    titles = [(idea.title.best_lang(user_prefs).value, idea.id) for idea in ideas]
+    titles.sort()
+    q = widget.db.query(Idea.id).filter(Idea.id.in_(idea_ids))
+    specs = [(spec.title.best_lang(user_prefs).value if spec.title else str(spec.id), spec)
+             for spec in widget.vote_specifications]
+    specs.sort()
+    for (t, spec) in specs:
+        a = aliased(spec.get_vote_class(), name="votes_%d"%spec.id)
+        q = q.outerjoin(a, (a.idea_id==Idea.id) & (a.vote_spec_id==spec.id))
+        q = q.add_columns(func.sum(a.vote_value).label('vsum_%d' % spec.id),
+                          func.count(a.id).label('vcount_%d' % spec.id))
+    q = q.group_by(Idea.id)
+    r = q.all()
+    r = {x[0]: x for x in r}
+    output = StringIO()
+    csvw = csv.writer(output)
+    csvw.writerow([""]+[t.encode('utf-8') for (t, spec) in specs])
+    for title, idea_id in titles:
+        row = [title.encode('utf-8')]
+        sourcerow = r[idea_id][1:]
+        for i, (t, spec) in enumerate(specs):
+            num = sourcerow[1+i*2]
+            if num:
+                if isinstance(spec, TokenVoteSpecification):
+                    num = 1
+                row.append(sourcerow[i*2]/num)
+            else:
+                row.append("")
+        csvw.writerow(row)
     output.seek(0)
     return Response(body_file=output, content_type='text/csv')

conversence/idealoom

assembl/views/api2/votes.py
@@ -2,6 +2,7 @@ from future import standard_library
 standard_library.install_aliases()
 from datetime import datetime
 from io import BytesIO, TextIOWrapper
+import csv
 
 from pyramid.view import view_config
 from pyramid.httpexceptions import (
@@ -10,12 +11,15 @@ from pyramid.security import authenticated_userid
 from pyramid.response import Response
 from pyramid.settings import asbool
 from simplejson import dumps
+from sqlalchemy.sql import func
+from sqlalchemy.orm import aliased
 
 from ..traversal import (CollectionContext, InstanceContext)
 from assembl.auth import (
-    P_READ, Everyone, CrudPermissions, P_ADMIN_DISC, P_VOTE)
+    P_READ, Everyone, CrudPermissions, P_ADMIN_DISC, P_VOTE, P_DISC_STATS)
 from assembl.models import (
-    AbstractIdeaVote, User, AbstractVoteSpecification, VotingWidget)
+    Idea, AbstractIdeaVote, User, AbstractVoteSpecification, VotingWidget,
+    TokenVoteSpecification, LanguagePreferenceCollection)
 from assembl.lib.sqla import get_named_class
 from . import (FORM_HEADER, JSON_HEADER, check_permissions)
 
@@ -122,7 +126,7 @@ def vote_results(request):
 
 @view_config(context=InstanceContext, request_method='GET',
              ctx_instance_class=AbstractVoteSpecification,
-             name="vote_results_csv", permission=P_READ)
+             name="vote_results_csv", permission=P_DISC_STATS)
 def vote_results_csv(request):
     ctx = request.context
     user_id = authenticated_userid(request)
@@ -145,6 +149,68 @@ def vote_results_csv(request):
     output = BytesIO()
     output_utf8 = TextIOWrapper(output, encoding='utf-8')
     ctx._instance.csv_results(output_utf8, histogram)
+    output_utf8.detach()
+    output.seek(0)
+    return Response(body_file=output, content_type='text/csv', charset="utf-8")
+
+
+@view_config(context=InstanceContext, request_method='GET',
+             ctx_instance_class=VotingWidget,
+             name="vote_results_csv", permission=P_DISC_STATS)
+def global_vote_results_csv(request):
+    ctx = request.context
+    user_id = request.authenticated_userid
+    if not user_id:
+        raise HTTPUnauthorized
+    widget = ctx._instance
+    if widget.activity_state != "ended":
+        permissions = ctx.get_permissions()
+        if P_ADMIN_DISC not in permissions:
+            raise HTTPUnauthorized()
+    user_prefs = LanguagePreferenceCollection.getCurrent()
+    # first fetch the ideas voted on
+    ideas = widget.db.query(Idea
+        ).join(AbstractIdeaVote, AbstractIdeaVote.idea_id == Idea.id
+        ).join(AbstractVoteSpecification
+        ).filter(AbstractVoteSpecification.widget_id == widget.id
+        ).distinct().all()
+    idea_ids = [i.id for i in ideas]
+    titles = [(idea.safe_title(user_prefs, request.localizer), idea.id) for idea in ideas]
+    titles.sort()
+    q = widget.db.query(Idea.id).filter(Idea.id.in_(idea_ids))
+    # then get the vote specs
+    specs = [(spec.title.best_lang(user_prefs).value if spec.title else str(spec.id), spec)
+             for spec in widget.vote_specifications]
+    specs.sort()
+    # construct a query with each votespec creating two columns:
+    # sum of vote values, and count of votes.
+    # Ideas are rows (and Idea.id is column 0)
+    for (t, spec) in specs:
+        a = aliased(spec.get_vote_class(), name="votes_%d" % spec.id)
+        q = q.outerjoin(a, (a.idea_id == Idea.id) & (a.vote_spec_id == spec.id))
+        q = q.add_columns(func.sum(a.vote_value).label('vsum_%d' % spec.id),
+                          func.count(a.id).label('vcount_%d' % spec.id))
+    q = q.group_by(Idea.id)
+    r = q.all()
+    r = {x[0]: x for x in r}
+    output = BytesIO()
+    output_utf8 = TextIOWrapper(output, encoding='utf-8')
+    csvw = csv.writer(output_utf8)
+    csvw.writerow([""] + [t.encode('utf-8') for (t, spec) in specs])
+    for title, idea_id in titles:
+        row = [title.encode('utf-8')]
+        sourcerow = r[idea_id][1:]
+        for i, (t, spec) in enumerate(specs):
+            num = sourcerow[1 + i * 2]
+            if num:
+                if isinstance(spec, TokenVoteSpecification):
+                    # we want total number of tokens
+                    num = 1
+                # otherwise we want average vote value
+                row.append(sourcerow[i * 2] / num)
+            else:
+                row.append("")
+        csvw.writerow(row)
     output_utf8.detach()
     output.seek(0)
     return Response(body_file=output, content_type='text/csv', charset="utf-8")