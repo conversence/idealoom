assembl/assembl

rewrite nginx template. Make it executable from rc files. Add ini_files command. Rename changes.* configs to changes_* so they can be used in jinja.

assembl/tasks/changes_router.py
@@ -32,12 +32,12 @@ if len(sys.argv) != 2:
 
 SECTION = 'app:assembl'
 
-settings = ConfigParser.ConfigParser({'changes.prefix': ''})
+settings = ConfigParser.ConfigParser({'changes_prefix': ''})
 settings.read(sys.argv[-1])
-CHANGES_SOCKET = settings.get(SECTION, 'changes.socket')
-CHANGES_PREFIX = settings.get(SECTION, 'changes.prefix')
+CHANGES_SOCKET = settings.get(SECTION, 'changes_socket')
+CHANGES_PREFIX = settings.get(SECTION, 'changes_prefix')
 TOKEN_SECRET = settings.get(SECTION, 'session.secret')
-WEBSERVER_PORT = settings.getint(SECTION, 'changes.websocket.port')
+WEBSERVER_PORT = settings.getint(SECTION, 'changes_websocket_port')
 # NOTE: Not sure those are always what we want.
 SERVER_HOST = settings.get(SECTION, 'public_hostname')
 SERVER_PORT = settings.getint(SECTION, 'public_port')

conversence/idealoom

assembl/tasks/changes_router.py
@@ -156,14 +156,14 @@ if __name__ == '__main__':
 
     logging.config.fileConfig(sys.argv[1],)
 
-    settings = configparser.ConfigParser({'changes.prefix': ''})
+    settings = configparser.ConfigParser({'changes_prefix': ''})
     settings.read(sys.argv[-1])
 
 
-    changes_socket = settings.get(SECTION, 'changes.socket')
-    changes_prefix = settings.get(SECTION, 'changes.prefix')
+    changes_socket = settings.get(SECTION, 'changes_socket')
+    changes_prefix = settings.get(SECTION, 'changes_prefix')
     TOKEN_SECRET = settings.get(SECTION, 'session.secret')
-    websocket_port = settings.getint(SECTION, 'changes.websocket.port')
+    websocket_port = settings.getint(SECTION, 'changes_websocket_port')
     # NOTE: Not sure those are always what we want.
     REQUIRES_SECURE = settings.getboolean(SECTION, 'require_secure_connection')
     SERVER_PROTOCOL = 'https' if REQUIRES_SECURE else 'http'