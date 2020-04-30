ee54c70d052e8403b0fb32d71542bb07f83dadf8
Marc-Antoine Parent <maparent@acm.org>
2020-04-30 14:39:23 -0400

    also requirements, remove pyoembed from pyramid

--- assembl/views/api/utils.py
+++ assembl/views/api/utils.py
@@ -5,27 +5,16 @@ from builtins import str
 import re
 from urllib.parse import urlparse
 from os import path
-from os.path import join, dirname
 
 import requests
 from pyramid.httpexceptions import HTTPBadRequest
 from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid.security import NO_PERMISSION_REQUIRED
-from pyramid_dogpile_cache import get_region
-from pyoembed import oEmbed
 
 from assembl.lib import config
 from assembl.models import File
-from assembl.lib.config import get_config
 
-
-dogpile_fname = join(
-    dirname(dirname(dirname(dirname(__file__)))),
-    get_config().get('dogpile_cache.arguments.filename'))
-
-oembed_cache = get_region(
-    'oembed', **{"arguments.filename": dogpile_fname})
 
 
 @view_config(route_name='mime_type', request_method='HEAD',
@@ -47,7 +36,6 @@ def mime_type(request):
             url)
         if r:
             document_id = r.groups(0)[0]
-            from sqlalchemy.sql.functions import func
             mimetype, create_date, file_identity = File.default_db.query(
                 File.mime_type, File.creation_date, File.file_identity
                 ).filter_by(id=int(document_id)).first()
@@ -66,23 +54,3 @@ def mime_type(request):
         content_type=result.headers.get('Content-Type', None),
         status=result.status_code,
         location=result.url)
-
-
-@oembed_cache.cache_on_arguments()
-def oembed_cached(url, maxwidth=None, maxheight=None):
-    return oEmbed(url, maxwidth=None, maxheight=maxheight)
-
-
-@view_config(
-    route_name='oembed', permission=NO_PERMISSION_REQUIRED, renderer='json')
-def oembed(request):
-    url = request.params.get('url', None)
-    if not url:
-        raise HTTPBadRequest("Please give a URL")
-    maxheight = request.params.get('height', None)
-    maxwidth = request.params.get('width', None)
-    try:
-        return oembed_cached(url, maxwidth, maxheight)
-    except:
-        # Cache failures
-        return {}
